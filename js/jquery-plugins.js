17c5f9da420e75a4376a252fe801238a0acffe23
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-07 14:32:23 -0600

    first commit

---
+++ js/jquery-plugins.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    FastClick.attach(document.body); // remove 300ms delay of click on mobile
+    $('button').nodoubletapzoom(); // remove double tap zoon on iOS jQuery plugin   
+
+
+
+});
