2cdf3b0eab1f6a490bd92bcb9e122249f1dec585
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-27 12:44:44 -0600

    emailjs setup and form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,84 @@
 $(document).ready(function () {
     $('#submit-btn').click(function (e) {
         e.preventDefault();  //prevent page reload
-        console.log('click');
-  /*      emailjs.send('gmail', 'template_wyC99JVG', {
-            name: "James",
-            notes: "Check this out!"
-        })
-            .then(
-            function (response) {
-                console.log('SUCCESS', response);
-            },
-            function (error) {
-                console.log('FAILED', error);
+        $('.alert').hide();
+        /*      emailjs.send('gmail', 'template_wyC99JVG', {
+                  name: "James",
+                  notes: "Check this out!"
+              })
+                  .then(
+                  function (response) {
+                      console.log('SUCCESS', response);
+                  },
+                  function (error) {
+                      console.log('FAILED', error);
+                  }
+                  ); */
+
+        var senderEmail = document.getElementById("senderEmail").value;
+        var senderSubject = document.getElementById("senderSubject").value;
+        var senderMessage = document.getElementById("senderInputMessage").value;
+        console.log(senderEmail, senderSubject, senderMessage);
+        var emailRegEx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        function validate() {
+            var count = 0;
+            if (emailRegEx.test(senderEmail)) {  //validate email address
+                count += 1;
+                console.log('valid');
             }
-            ); */
-            var senderEmail = document.getElementById("senderEmail").value;
-            var senderSubject= document.getElementById("senderSubject").value;
-            var senderMessage = document.getElementById("senderInputMessage").value;
-            console.log(senderEmail, senderSubject, senderMessage);
-
-
-
-
+            else {
+                $('.alert').text('Please enter a valid email address.');
+                $('.alert').show();
+                $('#senderEmail').focus();
+                return false;
+            }
+            if (senderSubject === "") {
+                $('.alert').text('Please enter a subject for your message.');
+                $('.alert').show();
+                $('#senderSubject').focus();
+                return false;
+            }
+            else {
+                count += 1;
+            }
+            if (senderMessage === "") {
+                $('.alert').text('Please enter text for your message.');
+                $('.alert').show();
+                $('#senderMessage').focus();
+                return false;
+            }
+            else {
+                count += 1;
+            }
+            if (count === 3) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        var result = validate();
+        console.log(result);
+        if (result) {
+            console.log("send email!");
+            /*      emailjs.send('gmail', 'template_wyC99JVG', {
+                        sender: senderEmail,
+                        subject: senderSubject,
+                        message: senderMessage
+                    })
+                        .then(
+                        function (response) {
+                            console.log('SUCCESS', response);
+                        },
+                        function (error) {
+                            console.log('FAILED', error);
+                        }
+                        ); */
+                        $('#contactForm').each(function(){
+                            this.reset();
+                        });
+        }
 
     });
 
